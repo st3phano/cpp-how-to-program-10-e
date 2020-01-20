4fa7bd0d77f14e91e242e28516b6b836d8004cd5
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-20 13:43:52 -0300

    Create 11.cpp

---
+++ chapter-2/11.cpp
@@ -0,0 +1,14 @@
+//Print the numbers from 1 to 4
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "1 2 3 4\n";
+    cout << "1 " << "2 " << "3 " << "4\n";
+
+    cout << "1 ";
+    cout << "2 ";
+    cout << "3 ";
+    cout << "4\n";
+}
