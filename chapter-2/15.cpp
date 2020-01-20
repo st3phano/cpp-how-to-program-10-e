bcc9588c7a9d1aaad7814c74b5f47996104acb33
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-20 16:21:15 -0300

    Create 15.cpp

---
+++ chapter-2/15.cpp
@@ -0,0 +1,16 @@
+//Prints a box, an oval, and arrow and a diamond
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "*********         ***          *               *      " << endl;
+    cout << "*       *       *     *       ***            *   *    " << endl;
+    cout << "*       *      *       *     *****          *     *   " << endl;
+    cout << "*       *      *       *       *           *       *  " << endl;
+    cout << "*       *      *       *       *          *         * " << endl;
+    cout << "*       *      *       *       *           *       *  " << endl;
+    cout << "*       *      *       *       *            *     *   " << endl;
+    cout << "*       *       *     *        *             *   *    " << endl;
+    cout << "*********         ***          *               *      " << endl;
+}
