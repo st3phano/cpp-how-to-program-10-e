35ea69f3e634acf0040d6ac45ae4a7daee23a8af
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-20 15:11:19 -0300

    Create 23.cpp

---
+++ chapter-2/23.cpp
@@ -0,0 +1,54 @@
+//Calculates and print a table of squares and cubes
+//of the integers from 0 to 10
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int integer{0};
+
+    cout << "integer\tsquare\tcube\n";
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 1;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 2;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 3;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 4;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 5;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 6;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 7;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 8;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 9;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+    integer = 10;
+    cout << integer << "\t" << integer * integer << "\t"
+         << integer * integer * integer << endl;
+
+}
