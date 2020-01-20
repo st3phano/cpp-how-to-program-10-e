eb8cbe4be6d12ae95a189734061d626631859c37
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-20 15:48:00 -0300

    Create 04.cpp

---
+++ chapter-2/04.cpp
@@ -0,0 +1,5 @@
+1) False, the assigment operator is evaluated from right to left
+2) True
+3) False, since "a = 5;" is inside quotes it is a string.
+4) False, there is a table of precedence that should be followed first.
+5) False, h22 is a valid variable name.
