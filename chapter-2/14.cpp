f89f5fe990e5764b4bf1fe7ad871901c0c081f67
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-20 16:00:30 -0300

    Create 14.cpp

---
+++ chapter-2/14.cpp
@@ -0,0 +1,16 @@
+//Read radius of a circle as an integer
+//prints diameter, circumference and area
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int radius{0};
+
+    cout << "Enter the radius as an integer: ";
+    cin >> radius;
+
+    cout << "Diameter: " << 2 * radius << endl;
+    cout << "Circumference: " << 2 * 3.14159 * radius << endl;
+    cout << "Area: " << 3.14159 * radius * radius << endl;
+}
