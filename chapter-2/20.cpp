e10233e8f0c0b2c97bba6c504821581359ec406d
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-20 16:29:32 -0300

    Create 20.cpp

---
+++ chapter-2/20.cpp
@@ -0,0 +1,18 @@
+//Prints a checkboard pattern in two different ways
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "******** " << endl;
+    cout << " ********" << endl;
+    cout << "******** " << endl;
+    cout << " ********" << endl;
+    cout << "******** " << endl;
+    cout << " ********" << endl;
+    cout << "******** " << endl;
+    cout << " ********" << endl;
+
+    cout << "******** \n ********\n******** \n ********\n"
+            "******** \n ********\n******** \n ********\n" << endl;
+}
