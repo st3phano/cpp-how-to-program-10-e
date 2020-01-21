23fbfebe755b0726046d948a12ff5d5196412676
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-21 08:59:04 -0300

    Update 04.cpp

--- chapter-2/04.cpp
+++ chapter-2/04.cpp
@@ -1,5 +1,5 @@
 1) False, the assigment operator is evaluated from right to left
 2) True
 3) False, since "a = 5;" is inside quotes it is a string.
-4) True
+4) False, the precedence of operators still have to be considered.
 5) False, h22 is a valid variable name.
