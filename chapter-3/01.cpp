27ca943b7703c0979dd03c334b000117c9a21ea8
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-22 10:41:50 -0300

    Create 01.cpp

---
+++ chapter-3/01.cpp
@@ -0,0 +1,3 @@
+1) A default constructor is the construtor used by a class when no custom costrutor is defined in its body.
+Object's fundamental types data members are not initialized by the default construtor,
+while data members that are object of other classes are inilialized by the class constructor they belong.  
