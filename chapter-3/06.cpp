1ac32dd4f67de9c283660dc72e679483d47905dd
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-22 14:08:26 -0300

    Create 06.cpp

---
+++ chapter-3/06.cpp
@@ -0,0 +1,65 @@
+// Invoice class with constructor, set and get functions
+#include <string>
+
+class Invoice {
+public:
+    Invoice(std::string number, std::string description, int amount, int cost) :
+        partNumber{ number }, partDescription{ description } {
+        if (amount > 0) {
+            quantity = amount;
+        }
+
+        if (cost > 0) {
+            price = cost;
+        }
+    }
+
+    void setPartNumber(std::string number) {
+        partNumber = number;
+    }
+
+    void setPartDescription(std::string description) {
+        partDescription = description;
+    }
+
+    void setQuantity(int amount) {
+        if (amount > 0) {
+            quantity = amount;
+            return;
+        }
+        quantity = 0;
+    }
+
+    void setPrice(int cost) {
+        if (cost > 0) {
+            price = cost;
+            return;
+        }
+        price = 0;
+    }
+
+    std::string getPartNumber() const {
+        return partNumber;
+    }
+
+    std::string getPartDescription() const {
+        return partDescription;
+    }
+
+    int getQuantity() const {
+        return quantity;
+    }
+
+    int getPrice() const {
+        return price;
+    }
+
+    int getInvoiceAmount() const {
+        return quantity * price;
+    }
+private:
+    std::string partNumber;
+    std::string partDescription;
+    int quantity{ 0 };
+    int price{ 0 };
+};
