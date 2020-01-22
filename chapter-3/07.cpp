8d181b51a00cd4a2337f54e39eaa0a76988f1ae1
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-22 18:15:48 -0300

    Create 07.cpp

---
+++ chapter-3/07.cpp
@@ -0,0 +1,61 @@
+//Employee class with a demonstration
+#include <string>
+
+class Employee {
+public:
+	Employee(std::string firstN, std::string lastN, int amount)
+		: firstName{firstN}, lastName{lastN} {
+		if (amount > 0) {
+			salary = amount;
+		}
+	}
+
+	void setFirstName(std::string name) {
+		firstName = name;
+	}
+
+	void setLastName(std::string name) {
+		lastName = name;
+	}
+
+	void setSalary(int amount) {
+		if (amount > 0) {
+			salary = amount;
+		}
+	}
+
+	std::string getFirstName() const {
+		return firstName;
+	}
+
+	std::string getLastName() const {
+		return lastName;
+	}
+
+	int getSalary() const {
+		return salary;
+	}
+
+private:
+	std::string firstName;
+	std::string lastName;
+	int salary;
+}; //end of Employee class
+
+#include <iostream>
+
+int main() {
+	Employee bob{"Bob", "Silva", 1500};
+	Employee john{"John", "Smith", 1200};
+
+	std::cout << bob.getFirstName() << " earns " << 12 * bob.getSalary() << " in a year." << std::endl;
+	std::cout << john.getFirstName() << " earns " << 12 * john.getSalary() << " in a year." << std::endl;
+
+	bob.setSalary(bob.getSalary() * 1.1);
+	john.setSalary(john.getSalary() * 1.1);
+
+	std::cout << "After a 10% raise:\n";
+
+	std::cout << bob.getFirstName() << " earns " << 12 * bob.getSalary() << " in a year." << std::endl;
+	std::cout << john.getFirstName() << " earns " << 12 * john.getSalary() << " in a year." << std::endl;
+}
