2b77c13eec72365bfcb12bd5a6a0ed7f86ef2d1d
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-24 16:16:06 -0300

    Create 25-1.cpp

---
+++ chapter-4/25-1.cpp
@@ -0,0 +1,21 @@
+//Computes the factorial
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Enter a number to take the factorial: ";
+	unsigned int number;
+	cin >> number;
+
+	cout << number << "! = ";
+
+	unsigned int factorial{1};
+	if (number > 1) {
+		while (number > 1) {
+			factorial *= number--;
+		}
+	}
+
+	cout << factorial << endl;
+}
