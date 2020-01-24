7b546727185065e9dd6ecb8f612adf752137d06e
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-24 12:18:36 -0300

    Create 17.cpp

---
+++ chapter-4/17.cpp
@@ -0,0 +1,24 @@
+//Determine if a integer is a palindrome
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Enter a positive integer: ";
+	unsigned int number;
+	cin >> number;
+	unsigned int copyOfNumber{number};
+
+	unsigned int inverseNumber{0};
+	while (number > 0) {
+		inverseNumber = inverseNumber * 10 + number % 10;
+		number /= 10;
+	}
+
+	if (copyOfNumber == inverseNumber) {
+		cout << copyOfNumber << " is a palindrome.\n";
+	}
+	else {
+		cout << copyOfNumber << " is NOT a palindrome.\n";
+	}
+}
