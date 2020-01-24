ce461d9a7d64734ffd7e37400cebfa0b44b12197
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-24 16:45:11 -0300

    Create 25-2.cpp

---
+++ chapter-4/25-2.cpp
@@ -0,0 +1,27 @@
+//Estimates the value of mathematical e
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "How many terms to use in the summation: ";
+	unsigned int terms;
+	cin >> terms;
+	unsigned int precision{terms};
+
+	double e{1};
+	while (terms > 0) {
+
+		double factorial{1};
+		unsigned int currentTerm{terms};
+		while (currentTerm > 1) {
+			factorial *= currentTerm;
+			--currentTerm;
+		}
+
+		e += 1 / factorial;
+		--terms;
+	}
+
+	cout << "e is approximately " << e << endl;
+}
