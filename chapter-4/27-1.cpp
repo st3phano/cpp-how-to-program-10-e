9eed9dbd5bed09a66385a441643730685304401f
st3phano <31676683+st3phano@users.noreply.github.com>
2020-01-25 12:35:55 -0300

    Create 27-1.cpp

---
+++ chapter-4/27-1.cpp
@@ -0,0 +1,31 @@
+//Encrypt four-digit integer
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Enter a four-digit integer: ";
+	unsigned int fourDigitInt;
+	cin >> fourDigitInt;
+
+	unsigned int digit1{fourDigitInt % 10};
+	fourDigitInt /= 10;
+	unsigned int digit2{fourDigitInt % 10};
+	fourDigitInt /= 10;
+	unsigned int digit3{fourDigitInt % 10};
+	fourDigitInt /= 10;
+	unsigned int digit4{fourDigitInt % 10};
+
+	digit1 = (digit1 + 7) % 10;
+	digit2 = (digit2 + 7) % 10;
+	digit3 = (digit3 + 7) % 10;
+	digit4 = (digit4 + 7) % 10;
+
+	unsigned int encryptedValue{0};
+	encryptedValue += digit3;
+	encryptedValue += digit4 * 10;
+	encryptedValue += digit1 * 100;
+	encryptedValue += digit2 * 1000;
+
+	cout << "Encrypted value: " << encryptedValue << endl;
+}
