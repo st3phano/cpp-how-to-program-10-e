3a9a484f07a742f61e7ea47402c3931d866d5e11
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-05 18:37:02 -0300

    Update 27-1.cpp

--- chapter-4/27-1.cpp
+++ chapter-4/27-1.cpp
@@ -14,9 +14,9 @@ int main() {
 	fourDigitInt /= 10;
 	unsigned int digit3{fourDigitInt % 10};
 	fourDigitInt /= 10;
-	unsigned int digit4{fourDigitInt % 10};
+	unsigned int digit4{fourDigitInt};
 
-	digit1 = (digit1 + 7) % 10;
+	digit1 = (digit1 + 7) digit1 > 10 ? % 10 : ;
 	digit2 = (digit2 + 7) % 10;
 	digit3 = (digit3 + 7) % 10;
 	digit4 = (digit4 + 7) % 10;
