4546a586a65ba7d9fe2c2cafe3a480eaa9cd2be4
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-05 18:44:11 -0300

    Update 27-2.cpp

--- chapter-4/27-2.cpp
+++ chapter-4/27-2.cpp
@@ -14,13 +14,13 @@ int main() {
 	encryptedValue /= 10;
 	unsigned int digit3{encryptedValue % 10};
 	encryptedValue /= 10;
-	unsigned int digit4{encryptedValue % 10};
+	unsigned int digit4{encryptedValue};
 
 	unsigned int decryptedValue{0};
-	decryptedValue += (digit3 + 3) % 10;
-	decryptedValue += (digit4 + 3) % 10 * 10;
-	decryptedValue += (digit1 + 3) % 10 * 100;
-	decryptedValue += (digit2 + 3) % 10 * 1000;
+	decryptedValue += digit3 + 3 > 10 ? (digit3 + 3) % 10 : digit3 + 3;
+	decryptedValue += digit4 + 3 > 10 ? (digit4 + 3) % 10 * 10 : digit4 + 3 * 10;
+	decryptedValue += digit1 + 3 > 10 ? (digit1 + 3) % 10 * 100 : digit1 + 3 * 100;
+	decryptedValue += digit2 + 3 > 10 ? (digit2 + 3) % 10 * 1000 : digit2 + 3 * 1000;
 
 	cout << "Decrypted value: " << decryptedValue << endl;
 }
