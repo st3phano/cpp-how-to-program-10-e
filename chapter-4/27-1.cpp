034eec608c3edac8c2d2e253db59efc66e5169a8
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-05 18:40:10 -0300

    Update 27-1.cpp

--- chapter-4/27-1.cpp
+++ chapter-4/27-1.cpp
@@ -16,10 +16,10 @@ int main() {
 	fourDigitInt /= 10;
 	unsigned int digit4{fourDigitInt};
 
-	digit1 = (digit1 + 7) digit1 > 10 ? % 10 : ;
-	digit2 = (digit2 + 7) % 10;
-	digit3 = (digit3 + 7) % 10;
-	digit4 = (digit4 + 7) % 10;
+	digit1 = digit1 + 7 > 10 ? (digit1 + 7) % 10 : digit1 + 7;
+	digit2 = digit2 + 7 > 10 ? (digit2 + 7) % 10 : digit2 + 7;
+	digit3 = digit3 + 7 > 10 ? (digit3 + 7) % 10 : digit3 + 7;
+	digit4 = digit4 + 7 > 10 ? (digit4 + 7) % 10 : digit4 + 7;
 
 	unsigned int encryptedValue{0};
 	encryptedValue += digit3;
