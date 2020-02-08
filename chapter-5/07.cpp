15ea556f00dc50d43ede96a289cd7ac2642a86b6
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-08 12:15:36 -0300

    Create 07.cpp

---
+++ chapter-5/07.cpp
@@ -0,0 +1,26 @@
+//Find the smallest integer
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Enter the number of values will be inputed: ";
+	unsigned int numberOfValues;
+	cin >> numberOfValues;
+
+	cout << "From now on enter a value and hit enter." << endl;
+	int smallestValue;
+	cin >> smallestValue;
+
+	for (unsigned int c{2}; c <= numberOfValues; ++c) {
+		int currentValue;
+		cin >> currentValue;
+
+		if (currentValue < smallestValue) {
+			smallestValue = currentValue;
+		}
+	}
+
+	cout << "Smallest value: " << smallestValue << endl;
+}
