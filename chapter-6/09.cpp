7c31abb723384e651cb149e9c4c1899812dce36b
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-19 12:51:57 -0300

    Update 09.cpp

--- chapter-6/09.cpp
+++ chapter-6/09.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-double hypotenuse(const double side1, const double side2);
+double hypotenuse(const double& side1, const double& side2);
 
 int main() {
 	cout << "Enter the two sides of a right triangle: ";
@@ -14,6 +14,6 @@ int main() {
 	cout << "The hypotenuse is " << hypotenuse(sideA, sideB) << endl;
 }
 
-double hypotenuse(const double side1, const double side2) {
+double hypotenuse(const double& side1, const double& side2) {
 	return sqrt(side1 * side1 + side2 * side2);
 }
