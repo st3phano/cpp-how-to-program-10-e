ac0a7c0cd6fd922fcc3ff84db4986d1fef185c8d
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-21 11:16:50 -0300

    Create 18.cpp

---
+++ chapter-6/18.cpp
@@ -0,0 +1,48 @@
+//Determine which numbers from 1 to 1000 are perfect numbers
+#include <iostream>
+
+using namespace std;
+
+bool isPerfect(const unsigned number);
+void printDivisors(const unsigned number);
+
+int main() {
+	const unsigned lastNumber{1000000000};
+
+	for (unsigned n{1}; n <= lastNumber; ++n) {
+		if (isPerfect(n)) {
+			printDivisors(n);
+		}
+	}
+}
+
+bool isPerfect(const unsigned number) {
+	unsigned divisor{number / 2};
+	unsigned sumOfDivisors{1};
+
+	while (divisor > 1) {
+		if (number % divisor == 0) {
+			sumOfDivisors += divisor;
+		}
+
+		--divisor;
+	}
+
+	return number == sumOfDivisors;
+}
+
+void printDivisors(const unsigned number) {
+	unsigned divisor{number / 2};
+
+	std::cout << number << " = ";
+
+	while (divisor > 1) {
+		if (number % divisor == 0) {
+			std::cout << divisor << " + ";
+		}
+
+		--divisor;
+	}
+
+	std::cout << 1 << std::endl;
+}
