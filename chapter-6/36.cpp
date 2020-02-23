bb1bbafbac969043bd55577fe8e0f55124fc6e82
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-23 18:33:20 -0300

    Create 36.cpp

---
+++ chapter-6/36.cpp
@@ -0,0 +1,9 @@
+1) The function definition needs a return type, float in this case.
+
+2) Replace srand() with rand().
+
+3) Change the type of "x" to float.
+
+4) Remove the line reinitializing the variable "number".
+
+5) Change the recursive call argument to "n - 1".
