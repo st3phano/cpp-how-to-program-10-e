bbd71ea6b9eb85671f32825d89b4db414bb744b0
st3phano <31676683+st3phano@users.noreply.github.com>
2020-02-23 12:09:01 -0300

    Update 31.cpp

--- chapter-6/31.cpp
+++ chapter-6/31.cpp
@@ -1,16 +1,5 @@
 //Determine the greatest common divisor using recursion
 //x must be larger than y
-#include <iostream>
-
-using namespace std;
-
-unsigned gcd(unsigned x, unsigned y);
-
-int main() {
-	cout << gcd(20, 3);
-
-}
-
 unsigned gcd(unsigned x, unsigned y) {
 	if (0 == y) {
 		return x;
